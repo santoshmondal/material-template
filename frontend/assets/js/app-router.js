82921e96df462e37acc7b3f14a48631e560bf1c5
santosh <santosh.ece06@gmail.com>
2017-04-24 13:06:34 +0530

    initial login commit.

--- frontend/assets/js/app-router.js
+++ frontend/assets/js/app-router.js
@@ -1,5 +1,21 @@
 var app = angular.module("app", ['ngMaterial', 'ui.router', 'ngMdIcons']);
 
+app.config(['$mdIconProvider', function($mdIconProvider) {
+    // SVG Icon
+    $mdIconProvider
+        .iconSet('social', 'assets/svg/social-icons.svg', 24)
+        .iconSet('image', 'assets/svg/image-icons.svg', 24)
+        .iconSet('action', 'assets/svg/action-icons.svg', 24)
+        .iconSet('file', 'assets/svg/file-icons.svg', 24)
+        .iconSet('hardware', 'assets/svg/hardware-icons.svg', 24)
+        .iconSet('navigation', 'assets/svg/navigation-icons.svg', 24)
+        .iconSet('alert', 'assets/svg/alert-icons.svg', 24)
+        .iconSet('content', 'assets/svg/content-icons.svg', 24)
+        .iconSet('editor', 'assets/svg/editor-icons.svg', 24)
+        .defaultIconSet("assets/svg/mdi.svg", 24);
+}]);
+
+
 app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise('/home');
 
