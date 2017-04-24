5fb7f0787fcdf0b4680001dcba294c25b2830177
santosh <santosh.ece06@gmail.com>
2017-04-24 12:51:30 +0530

    initial commit.

---
+++ frontend/assets/js/common-service.js
@@ -0,0 +1,8 @@
+var app = angular.module("app");
+
+app.service("COMMON_SERVICE", [function() {
+
+    // set on login
+    this.USER_META = {};
+
+}]);
--- frontend/assets/js/main.js
+++ frontend/assets/js/main.js
@@ -1,5 +1,21 @@
 var app = angular.module("app");
 
-app.controller("homeCtrl", ["$scope", function($scope) {
-    $scope.hello = "Hello World";
+app.controller("homeCtrl", ["$scope", "COMMON_SERVICE", function($scope, COMMON_SERVICE) {
+
+    // get userJson from localstorage;
+    $scope.onPageRefreshUpdateUserMeta = function() {
+        // READ FROM LOCAL STORAGE && RETURN USER META.
+        var userMetaStr = localStorage.getItem('USER_META');
+        var userMetaObj = angular.fromJson(userMetaStr);
+
+        if (userMetaObj && userMetaObj.user) {
+            $scope.LOGGED_IN = true;
+
+            COMMON_SERVICE.USER_META = userMetaObj;
+        } else {
+            $scope.LOGGED_IN = false;
+        }
+    };
+    $scope.onPageRefreshUpdateUserMeta();
+
 }]);
