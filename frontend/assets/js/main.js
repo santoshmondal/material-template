6b0f57d3e47c23ecdd41c4a5e6d719411c7d6de4
santosh <santosh.ece06@gmail.com>
2017-04-24 14:58:54 +0530

    initial login.

--- frontend/assets/js/main.js
+++ frontend/assets/js/main.js
@@ -7,11 +7,13 @@ app.controller("homeCtrl", ["$scope", "COMMON_SERVICE", function($scope, COMMON_
         console.log($scope.user);
 
         var user = $scope.user;
-        if (user.name == 'admin' && user.password == 'amdin') {
+        if (user.name == 'admin' && user.password == 'admin') {
             COMMON_SERVICE.USER_META = user;
 
             // save at local storage
             localStorage.setItem('USER_META', JSON.stringify(user));
+
+            $scope.LOGGED_IN = true;
         } else {
             user.errorMessage = "Authentication Fails";
         }
@@ -24,7 +26,7 @@ app.controller("homeCtrl", ["$scope", "COMMON_SERVICE", function($scope, COMMON_
         var userMetaStr = localStorage.getItem('USER_META');
         var userMetaObj = angular.fromJson(userMetaStr);
 
-        if (userMetaObj && userMetaObj.user) {
+        if (userMetaObj) {
             $scope.LOGGED_IN = true;
 
             COMMON_SERVICE.USER_META = userMetaObj;
